d1540c899d97dd7a8f311e4bf250eba7769a6964
Annie Cain <anniejocain@gmail.com>
2018-07-19 09:22:16 -0700

    add settings example file

---
+++ bot_settings.example.py
@@ -0,0 +1,11 @@
+CONNECT = {
+    'KEY_FILE_LOCATION': 'XXXXXXXXX.json',
+    'VIEW_ID': 'XXXXXXXXX',
+}
+
+SEGMENT = {
+    'FACULTY': 'text to match in page URL',
+    'STAFF': 'text to match in page URL',
+    'STUDENT': 'text to match in page URL',
+    'WELCOME': 'text to match in page URL',
+}
